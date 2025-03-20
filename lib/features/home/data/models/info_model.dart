8d0f24a1c3f09d2d26eb9fb70b483467ba3feacb
fady emil <fadymobiledev@gmail.com>
2025-03-20 19:00:21 +0200

    Add InfoModel and DataModel classes for user data serialization

---
+++ lib/features/home/data/models/info_model.dart
@@ -0,0 +1,47 @@
+import 'package:insta_app/features/home/domain/entites/data_info_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'info_model.g.dart';
+
+@JsonSerializable()
+class InfoModel {
+  final DataModel data;
+  InfoModel({required this.data});
+  factory InfoModel.fromJson(Map<String, dynamic> json) =>
+      _$InfoModelFromJson(json);
+  Map<String, dynamic> toJson() => _$InfoModelToJson(this);
+
+  DataInfoEntity toEntity() {
+    return DataInfoEntity(infoEntity: data.toEntity());
+  }
+}
+
+@JsonSerializable()
+class DataModel {
+  final String username;
+  final String full_name;
+  final int follower_count;
+  final int following_count;
+  final String profile_pic_url_hd;
+  final String biography;
+
+  DataModel(
+      {required this.username,
+      required this.full_name,
+      required this.follower_count,
+      required this.following_count,
+      required this.profile_pic_url_hd,
+      required this.biography});
+  factory DataModel.fromJson(Map<String, dynamic> json) =>
+      _$DataModelFromJson(json);
+  Map<String, dynamic> toJson() => _$DataModelToJson(this);
+  InfoEntity toEntity() {
+    return InfoEntity(
+      userName: username,
+      fullName: full_name,
+      followerCount: follower_count,
+      followingCount: following_count,
+      profilePicUrlJd: profile_pic_url_hd,
+      biography: biography,
+    );
+  }
+}
---
+++ lib/features/home/data/models/info_model.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'info_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+InfoModel _$InfoModelFromJson(Map<String, dynamic> json) => InfoModel(
+      data: DataModel.fromJson(json['data'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$InfoModelToJson(InfoModel instance) => <String, dynamic>{
+      'data': instance.data,
+    };
+
+DataModel _$DataModelFromJson(Map<String, dynamic> json) => DataModel(
+      username: json['username'] as String,
+      full_name: json['full_name'] as String,
+      follower_count: (json['follower_count'] as num).toInt(),
+      following_count: (json['following_count'] as num).toInt(),
+      profile_pic_url_hd: json['profile_pic_url_hd'] as String,
+      biography: json['biography'] as String,
+    );
+
+Map<String, dynamic> _$DataModelToJson(DataModel instance) => <String, dynamic>{
+      'username': instance.username,
+      'full_name': instance.full_name,
+      'follower_count': instance.follower_count,
+      'following_count': instance.following_count,
+      'profile_pic_url_hd': instance.profile_pic_url_hd,
+      'biography': instance.biography,
+    };
