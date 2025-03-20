29c56e8d708536cc127fa43ecec552cf595f176c
fady emil <fadymobiledev@gmail.com>
2025-03-20 23:22:12 +0200

    Update ImageAndStat and NameAndBio widgets to use DataInfoEntity for dynamic data

--- lib/features/user/presentation/widget/image_and_stat.dart
+++ lib/features/user/presentation/widget/image_and_stat.dart
@@ -1,11 +1,16 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:insta_app/features/auth/domain/entites/data_info_entity.dart';
 import 'package:insta_app/features/user/presentation/widget/build_stat_column.dart';
 
 class ImageAndStat extends StatelessWidget {
   const ImageAndStat({
     super.key,
+    required this.infoEntity,
   });
+
+  final DataInfoEntity infoEntity;
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +19,20 @@ class ImageAndStat extends StatelessWidget {
       children: [
         CircleAvatar(
           radius: 50.r,
-          backgroundImage: const AssetImage('assets/image/avatar.png'),
+          backgroundImage: CachedNetworkImageProvider(
+            infoEntity.infoEntity.profilePicUrlJd,
+          ),
+          onBackgroundImageError: (error, stackTrace) {
+            debugPrint("Error loading image: $error");
+          },
         ),
         BuildStatColumn(number: 45, title: 'Posts'),
         SizedBox(width: 5.w),
-        BuildStatColumn(number: 834, title: 'Followers'),
+        BuildStatColumn(
+            number: infoEntity.infoEntity.followerCount, title: 'Followers'),
         SizedBox(width: 5.w),
-        BuildStatColumn(number: 162, title: 'Following'),
+        BuildStatColumn(
+            number: infoEntity.infoEntity.followingCount, title: 'Following'),
       ],
     );
   }
--- lib/features/user/presentation/widget/name_and_bio.dart
+++ lib/features/user/presentation/widget/name_and_bio.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:insta_app/core/theme/app_text_styles.dart';
+import 'package:insta_app/features/auth/domain/entites/data_info_entity.dart';
 
 class NameAndBio extends StatelessWidget {
   const NameAndBio({
     super.key,
+    required this.dataInfoEntity,
   });
+
+  final DataInfoEntity dataInfoEntity;
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +19,14 @@ class NameAndBio extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "name",
+            dataInfoEntity.infoEntity.fullName,
             style: AppTextStyles.heading28Regular.copyWith(
               fontWeight: FontWeight.bold,
             ),
           ),
           SizedBox(height: 8.h),
           Text(
-            "biography",
+            dataInfoEntity.infoEntity.biography,
             style: AppTextStyles.bodyBasaRegular16.copyWith(
               color: Colors.grey[700],
             ),
