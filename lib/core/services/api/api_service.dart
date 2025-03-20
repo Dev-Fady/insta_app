5947844229261324a6d9cdf173857a4e9a19aad1
fady emil <fadymobiledev@gmail.com>
2025-03-20 18:59:21 +0200

    Add ApiService implementation using Retrofit for user info retrieval

---
+++ lib/core/services/api/api_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: "https://social-api4.p.rapidapi.com/v1/")
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET("info")
+  Future<dynamic> getUserInfo(
+    @Query("username_or_id_or_url") String username,
+    @Header("x-rapidapi-host") String host,
+    @Header("x-rapidapi-key") String apiKey,
+  );
+}
