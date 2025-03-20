b6805e547e711d1d95c38c74724da249e204018e
fady emil <fadymobiledev@gmail.com>
2025-03-20 19:00:06 +0200

    Add InfoRepo interface for user login functionality

---
+++ lib/features/home/domain/repo/info_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:insta_app/core/error/faliure.dart';
+import 'package:insta_app/features/home/domain/entites/data_info_entity.dart';
+
+abstract class InfoRepo {
+  Future<Either<Faliure, DataInfoEntity>> loginUser({required String userName});
+}
