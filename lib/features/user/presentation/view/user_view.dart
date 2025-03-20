baf4077abd594dd4db17f254ce0729a53f6160df
fady emil <fadymobiledev@gmail.com>
2025-03-20 23:22:27 +0200

    Refactor UserView to StatefulWidget; integrate BlocBuilder for LoginUserCubit state management and update app bar title dynamically based on user login state.

--- lib/features/user/presentation/view/user_view.dart
+++ lib/features/user/presentation/view/user_view.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:insta_app/core/cache/cache_helper.dart';
 import 'package:insta_app/core/theme/app_text_styles.dart';
 
+import '../../../auth/presentation/manger/cubit/login_user_cubit.dart';
 import '../widget/body_user_view.dart';
 
-class UserView extends StatelessWidget {
+class UserView extends StatefulWidget {
   const UserView({super.key});
 
   @override
+  State<UserView> createState() => _UserViewState();
+}
+
+class _UserViewState extends State<UserView> {
+  final String userName = CacheHelper().getData(key: 'userName');
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<LoginUserCubit>().loginUser(userName: userName);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          ' My name',
-          style: AppTextStyles.heading23Bold,
-        ),
-        centerTitle: true,
-      ),
-      body: BodyUserView(),
+    return BlocBuilder<LoginUserCubit, LoginUserState>(
+      builder: (context, state) {
+        if (state is LoginUserSuccess) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                state.dataInfoEntity.infoEntity.userName,
+                style: AppTextStyles.heading23Bold,
+              ),
+              centerTitle: true,
+            ),
+            body: BodyUserView(dataInfoEntity: state.dataInfoEntity),
+          );
+        } else if (state is LoginUserLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is LoginUserFailure) {
+          return Center(child: Text(state.errorMessage));
+        } else {
+          return const SizedBox.shrink();
+        }
+      },
     );
   }
 }
