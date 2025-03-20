c8ba57b06027c620baafc803ff1bd2289a586d25
fady emil <fadymobiledev@gmail.com>
2025-03-20 19:38:17 +0200

    Refactor BodyHomePage to use separate widgets for Instagram logo, username input, label, and login button

--- lib/features/home/presentation/view/widget/body_home_page.dart
+++ lib/features/home/presentation/view/widget/body_home_page.dart
@@ -3,19 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:insta_app/core/cache/cache_helper.dart';
-import 'package:insta_app/core/theme/app_text_styles.dart';
-import 'package:insta_app/core/widget/app_text_form_field.dart';
-import 'package:insta_app/core/widget/cusstom_button.dart';
 import 'package:insta_app/core/widget/custom_snackbar.dart';
 import 'package:insta_app/features/home/presentation/manger/cubit/login_user_cubit.dart';
-import 'package:ionicons/ionicons.dart';
+import 'package:insta_app/features/home/presentation/view/widget/instagram_logo.dart';
+import 'package:insta_app/features/home/presentation/view/widget/login_button.dart';
+import 'package:insta_app/features/home/presentation/view/widget/username_input_field.dart';
+import 'package:insta_app/features/home/presentation/view/widget/username_label.dart';
 
 import '../../../../../core/helper_functions/build_error_message.dart';
 
 class BodyHomePage extends StatelessWidget {
-  BodyHomePage({
-    super.key,
-  });
+  BodyHomePage({super.key});
 
   final TextEditingController userNameController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -32,61 +30,37 @@ class BodyHomePage extends StatelessWidget {
             type: ContentType.success,
           );
           await CacheHelper().saveData(key: 'isLogin', value: true);
-          // context.goNamed(RouterName.main_view);
-        }
-        if (state is LoginUserFailure) {
+        } else if (state is LoginUserFailure) {
           buildErrorBar(context, state.errorMessage);
         }
       },
       builder: (context, state) {
         return SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Form(
               key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 20.h,
-                children: [
-                  Center(
-                    child: Icon(
-                      Ionicons.logo_instagram,
-                      size: 100,
-                      color: Colors.black,
-                    ),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const InstagramLogo(),
+                      SizedBox(height: 20.h),
+                      const UsernameLabel(),
+                      SizedBox(height: 10.h),
+                      UsernameInputField(controller: userNameController),
+                      SizedBox(height: 20.h),
+                      if (state is LoginUserLoading)
+                        const CircularProgressIndicator()
+                      else
+                        LoginButton(
+                          formKey: formKey,
+                          controller: userNameController,
+                        ),
+                    ],
                   ),
-                  Text(
-                    "enter username",
-                    style: AppTextStyles.bodyBasaBold16,
-                  ),
-                  AppTextFormField(
-                    hintText: 'enter user name',
-                    keyboardType: TextInputType.name,
-                    controller: userNameController,
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return 'Username is required';
-                      }
-                      return null;
-                    },
-                  ),
-                  if (state is LoginUserLoading)
-                    Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  else
-                    CusstomButton(
-                      buttonText: 'enter ',
-                      textStyle: AppTextStyles.bodyBasaBold16,
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          context
-                              .read<LoginUserCubit>()
-                              .loginUser(userName: userNameController.text);
-                        }
-                      },
-                    )
-                ],
+                ),
               ),
             ),
           ),
