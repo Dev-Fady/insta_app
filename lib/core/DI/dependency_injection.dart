5bda8a4cb92dd8033830cc37dbf21705e412a4af
fady emil <fadymobiledev@gmail.com>
2025-03-21 14:49:28 +0200

    Register PostsRepo and PostsRepoImpl in dependency injection setup

--- lib/core/DI/dependency_injection.dart
+++ lib/core/DI/dependency_injection.dart
@@ -4,6 +4,8 @@ import 'package:insta_app/core/helper_functions/logging_interceptor.dart';
 import 'package:insta_app/core/services/api/api_service.dart';
 import 'package:insta_app/features/auth/data/repo/info_repo_impl.dart';
 import 'package:insta_app/features/auth/domain/repo/info_repo.dart';
+import 'package:insta_app/features/post/data/repo/posts_repo_impl.dart';
+import 'package:insta_app/features/post/domain/repo/posts_repo.dart';
 
 final getIt = GetIt.instance;
 
@@ -16,4 +18,7 @@ void setupGetit() {
 
   getIt.registerSingleton<InfoRepo>(
       InfoRepoImpl(apiService: getIt<ApiService>()));
+
+  getIt.registerSingleton<PostsRepo>(
+      PostsRepoImpl(apiService: getIt<ApiService>()));
 }
