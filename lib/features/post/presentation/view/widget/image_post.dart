a4e401b1cdfff64ff9444c15df82c12d8a7d2126
fady emil <fadymobiledev@gmail.com>
2025-03-22 12:16:02 +0200

    Enhance ImagePost widget to display a play icon overlay for video items

--- lib/features/post/presentation/view/widget/image_post.dart
+++ lib/features/post/presentation/view/widget/image_post.dart
@@ -1,8 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-import '../../../domain/entites/posts_entity.dart';
+import 'package:insta_app/features/post/domain/entites/posts_entity.dart';
 
 class ImagePost extends StatelessWidget {
   const ImagePost({
@@ -16,14 +14,36 @@ class ImagePost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      fit: BoxFit.cover,
-      imageUrl: postsEntityData.data.items[index].thumbnail_url ?? '',
-      placeholder: (context, url) =>
-          const Center(child: CircularProgressIndicator()),
-      errorWidget: (context, url, error) => Image.asset(
-        'assets/image/avatar.png',
-        fit: BoxFit.cover,
+    final isVideo = postsEntityData.data.items[index].is_video == true;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl: postsEntityData.data.items[index].thumbnail_url ?? '',
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => Image.asset(
+                'assets/image/avatar.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          if (isVideo)
+            Container(
+              color: Colors.black.withOpacity(0.3), // تعتيم الخلفية قليلاً
+              child: const Center(
+                child: Icon(
+                  Icons.play_circle_fill,
+                  color: Colors.white,
+                  size: 60,
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
