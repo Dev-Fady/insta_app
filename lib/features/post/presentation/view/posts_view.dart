5de59441fc1654325a330244318525c01b15e627
fady emil <fadymobiledev@gmail.com>
2025-03-22 15:38:19 +0200

    Refactor PostsView to use PostsRepo interface instead of PostsRepoImpl

--- lib/features/post/presentation/view/posts_view.dart
+++ lib/features/post/presentation/view/posts_view.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:insta_app/core/DI/dependency_injection.dart';
 import 'package:insta_app/core/cache/cache_helper.dart';
-import 'package:insta_app/core/services/api/api_service.dart';
-import 'package:insta_app/features/post/data/repo/posts_repo_impl.dart';
+import 'package:insta_app/features/post/domain/repo/posts_repo.dart';
 import 'package:insta_app/features/post/presentation/manger/cubit/posts_cubit.dart';
 
 import 'widget/body_posts_view.dart';
@@ -15,7 +14,7 @@ class PostsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => PostsCubit(
-        postsRepo: PostsRepoImpl(apiService: getIt<ApiService>()),
+        postsRepo: getIt<PostsRepo>(),
       )..getPosts(userName: CacheHelper().getData(key: 'userName')),
       child: BlocBuilder<PostsCubit, PostsState>(
         builder: (context, state) {
