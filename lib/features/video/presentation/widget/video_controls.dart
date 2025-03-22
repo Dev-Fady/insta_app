9edcbab4dee8ebe44f8befe3199ad6427a86628e
fady emil <fadymobiledev@gmail.com>
2025-03-22 11:21:30 +0200

    Refactor VideoControls to use StatefulWidget for better state management and update play/pause functionality

--- lib/features/video/presentation/widget/video_controls.dart
+++ lib/features/video/presentation/widget/video_controls.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class VideoControls extends StatelessWidget {
+class VideoControls extends StatefulWidget {
   final VideoPlayerController controller;
 
   const VideoControls({Key? key, required this.controller}) : super(key: key);
 
-  void _rewind5Sec() => controller.seekTo(controller.value.position - const Duration(seconds: 5));
-  void _forward5Sec() => controller.seekTo(controller.value.position + const Duration(seconds: 5));
+  @override
+  State<VideoControls> createState() => _VideoControlsState();
+}
+
+class _VideoControlsState extends State<VideoControls> {
+  void _rewind5Sec() => widget.controller
+      .seekTo(widget.controller.value.position - const Duration(seconds: 5));
+
+  void _forward5Sec() => widget.controller
+      .seekTo(widget.controller.value.position + const Duration(seconds: 5));
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +23,38 @@ class VideoControls extends StatelessWidget {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          IconButton(icon: const Icon(Icons.replay_5, size: 40, color: Colors.white), onPressed: _rewind5Sec),
           IconButton(
-            icon: Icon(controller.value.isPlaying ? Icons.pause : Icons.play_arrow, size: 40, color: Colors.white),
-            onPressed: () => controller.value.isPlaying ? controller.pause() : controller.play(),
+              icon: const Icon(
+                Icons.replay_5,
+                size: 40,
+                color: Colors.white,
+              ),
+              onPressed: _rewind5Sec),
+          IconButton(
+            icon: Icon(
+                widget.controller.value.isPlaying
+                    ? Icons.pause
+                    : Icons.play_arrow,
+                size: 40,
+                color: Colors.white),
+            onPressed: () {
+              setState(() {
+                if (widget.controller.value.isPlaying) {
+                  widget.controller.pause();
+                } else {
+                  widget.controller.play();
+                }
+              });
+            },
           ),
-          IconButton(icon: const Icon(Icons.forward_5, size: 40, color: Colors.white), onPressed: _forward5Sec),
+          IconButton(
+            icon: const Icon(
+              Icons.forward_5,
+              size: 40,
+              color: Colors.white,
+            ),
+            onPressed: _forward5Sec,
+          ),
         ],
       ),
     );
